766a52ee0c6f86beeb2fa89d614e51a985a979ff
sam <sam@LAPTOP-7UAA9O2J>
2019-06-14 15:51:44 +0200

    modifica struttura classi

---
+++ com/project/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.project;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectApplicationTests {
+
+	@Test()
+	public void contextLoads() {
+		// TODO - implement ProjectApplicationTests.contextLoads
+		throw new UnsupportedOperationException();
+	}
+
+}
