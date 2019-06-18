09396df524e06fb4f86b45c29f9dfcd30ce04bd6
sam <sam@LAPTOP-7UAA9O2J.homenet.telecomitalia.it>
2019-06-18 09:17:33 +0200

    Modifica dei filtri e aggiunta dei commenti

--- Project/src/main/java/com/esame/project/Controller.java
+++ Project/src/main/java/com/esame/project/Controller.java
@@ -16,36 +16,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import org.json.simple.*;
 
 @RestController 
 public class Controller {
-    //stampa tutti i dati del dataset in formato json
+	//stampa tutti i dati del dataset in formato json
 	@RequestMapping(path="/data", method = RequestMethod.GET, headers="Accept=application/json; charset=utf-8")
-	public List<DatasetStructure> GetLista() throws FileNotFoundException, IOException //stampa tutti i dati del dataset in formato json
+	public List<DatasetStructure> GetAllData() throws FileNotFoundException, IOException 
 	{
 		ListGenerator Lista = new ListGenerator();
 		return Lista.getLista();
 	}
-	@RequestMapping(path="/metadata", method = RequestMethod.GET, headers="Accept=application/json")
+	//stampa i dati del filtro scelto
+	@RequestMapping(path="/data/filtro", method = RequestMethod.GET, headers="Accept=application/json; charset=utf-8")
+	public List<DatasetStructure> Filtri(@RequestParam(value="TipoAttivita",required=false) String tipo
+										, @RequestParam(value="Camere",required=false)Integer camere
+										,@RequestParam(value="Municipio",required=false)Integer municipio) throws FileNotFoundException, IOException //stampa tutti i dati del dataset in formato json
+	{
+		// esempio di filtro: http://localhost:8080/data/filtro?TipoAttivita=Affittacamere&Camere=2&Municipio=1
+		return FilterUtils.filtro(tipo,camere,municipio); //filtro per tipo di attività, numero di camere e zona(municipio)
+	}
 	//stampa metadata in formato json
+	@RequestMapping(path="/metadata", method = RequestMethod.GET, headers="Accept=application/json")
 	public JSONArray GetMetadata() throws FileNotFoundException, IOException, ClassNotFoundException
 	{
 		MetadataGenerator metadata = new MetadataGenerator();
 		return metadata.getMetadata();
 	}
-	@RequestMapping(path="/filtro", method = RequestMethod.GET, headers="Accept=application/json")
-	//stampa risultati dei filtri applicati in formato json
-	public @ResponseBody ArrayList<DatasetStructure> Getfiltri() throws IOException, ClassNotFoundException
-	{
-		ListGenerator lista = new ListGenerator();
-		ArrayList<DatasetStructure> pout = lista.filterField("TipoAttivita", "=", "Affittacamere");//esempio di filtro
-		return pout;	
-	}
-	@RequestMapping(path="/map", method = RequestMethod.GET, headers="Accept=application/json")
 	//stampa mappa in formato geojson
+	@RequestMapping(path="/map", method = RequestMethod.GET, headers="Accept=application/json")
 	public JSONArray getMap () throws ParseException, IOException
 	{   
-		MapParsing map = new MapParsing();	
+		MapFilter map = new MapFilter();	
 		return map.getMap();
 	}
 
--- Project/src/main/java/com/esame/project/FilterUtils.java
+++ Project/src/main/java/com/esame/project/FilterUtils.java
@@ -1,26 +1,13 @@
 package com.esame.project;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class FilterUtils<T> {
-	public static boolean check(Object value, String operator, Object th) {
-		if (th instanceof Number && value instanceof Number) {	
-			Double thC = ((Number)th).doubleValue();
-			Double valuec = ((Number)value).doubleValue();
-			if (operator.equals("=="))
-				return value.equals(th);
-			else if (operator.equals(">"))
-				return valuec > thC;
-			else if (operator.equals("<"))
-				return valuec < thC;
-		}else if(th instanceof String && value instanceof String)
-			return value.equals(th);
-		return false;		
-	}
 	
 	public Collection<T> select(Collection<T> src, String fieldName, String operator, Object value) {
 		Collection<T> out = new ArrayList<T>();
@@ -47,8 +34,73 @@ public class FilterUtils<T> {
 			} catch (SecurityException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}					
+			}
 		}
 		return out;
 	}
+	
+	public static boolean check(Object value, String operator, Object th) {
+		if (th instanceof Number && value instanceof Number) {	//se si stanno confrontando 2 numeri
+			Double thC = ((Number)th).doubleValue();        //faccio il casting di tipo double
+			Double valuec = ((Number)value).doubleValue();  //faccio il casting di tipo double
+			if (operator.equals("="))				//se l'operatore è uguale
+				return value.equals(th);            //ritorna vero se è maggiore sennò falso
+			else if (operator.equals(">"))          //se l'operatore è maggiore
+				return valuec > thC;                //ritorna vero se è maggiore sennò falso
+				else if (operator.equals("<"))       //se l'operatore è minore
+					return valuec < thC;            //ritorna vero se è minore sennò falso
+		}else if(th instanceof String && value instanceof String) //se si stanno confrontando 2 stringhe
+			return value.equals(th);       //ritorna vero se sono uguali sennò falso
+		return false;		//in tutti gli altri casi ritorna falso
+	}
+	
+	public static List<DatasetStructure> filtro (String tipo, Integer camere, Integer municipio ) throws FileNotFoundException, IOException
+	{
+		ListGenerator Lista = new ListGenerator();
+		ArrayList<DatasetStructure> filtroTipo = new ArrayList();
+		ArrayList<DatasetStructure> filtroCamere = new ArrayList();
+		ArrayList<DatasetStructure> filtroMunicipio = new ArrayList();
+		//cambio i nomi per cercare alcuni parametri
+		if (tipo != null)   
+		{
+			if(tipo.equals("BedeBreakfast")) tipo="Bed e Breakfast"; 
+			if(tipo.equals("CasaFerie")) tipo="Casa Ferie";
+		}
+		//fine cambio nomi
+		
+		filtroTipo = Lista.filterField("TipoAttivita", "=", tipo);  //filtro i tipi di attività
+		filtroCamere = Lista.filterField("Camere", "=",camere);     //filtro i numeri delle camere
+		filtroMunicipio = Lista.filterField("Municipio", "=",municipio); //filtro i municipi
+        
+		//valuto caso per caso il filtro da attuare a seconda della scelta dell'utente
+		if(tipo!=null && camere==null && municipio==null) return filtroTipo;
+		else if(tipo==null && camere!=null && municipio==null) return filtroCamere;
+		else if(tipo==null && camere==null && municipio!=null) return filtroMunicipio;
+		else if(tipo!=null || camere!=null || municipio!=null) 
+		{
+			//filtri multipli: in questo caso devo  prendere l'intersezione degli elementi ottenuti
+			if(tipo!=null&&camere!=null&&municipio==null)
+			{
+				List Intersezione=new ArrayList(filtroTipo);
+				Intersezione.retainAll(filtroCamere);
+				return Intersezione;
+			}
+			if(tipo!=null&&municipio!=null&&municipio==null)
+			{
+				List Intersezione=new ArrayList(filtroTipo);
+				Intersezione.retainAll(filtroMunicipio);
+				return Intersezione;
+			}
+			if(tipo!=null&&camere!=null&&municipio!=null)
+			{
+				List Intersezione=new ArrayList(filtroTipo);
+				Intersezione.retainAll(filtroCamere);
+				List Intersezione2=new ArrayList(filtroMunicipio);
+				Intersezione2.retainAll(Intersezione);
+				return Intersezione2;
+			}
+		}
+	 return Lista.getLista(); // in tutti gli altri casi, ovvero quando il filtro 
+	}
+
 }
